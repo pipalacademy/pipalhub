c2ff9580c38dbb98c816344798614d0507b17706
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-11-13 16:45:25 +0530

    post_save_hook: Fix permissions, symlink notebook to notebooks directory
    Each post save script is run as the user that is trying to save it. Previously,
    everything used to be done by a single user. These saves or directory creations
    are by default made with a permissions mask that disallows other users from
    editing them. This was a problem for shared directories. This commit fixes that.

--- etc/jupyter/jupyter_notebook_config.py
+++ etc/jupyter/jupyter_notebook_config.py
@@ -1,6 +1,8 @@
 _script_exporter = None
+import contextlib
 import io
 import os
+import re
 from notebook.utils import to_api_path
 
 EXPORT_DIR = "/tmp/tmp/build/build/"
@@ -23,6 +25,30 @@ def create_parent_dirs(path):
     parent_dir = os.path.dirname(path)
     os.makedirs(parent_dir, exist_ok=True)
 
+re_notebook_path = re.compile("/home/([^/]+)/(.*)\.ipynb")
+def find_dest_path(src_path, output_dir):
+    m = re_notebook_path.match(src_path)
+    if m:
+        username, path = m.groups()
+        html_path = "{}/{}.html".format(path, username)
+    else:
+        html_path = src_path.replace(".ipynb", ".html")
+    return os.path.join(output_dir, html_path)
+
+def find_symlink_path(src_path):
+    dest_path = find_dest_path(src_path, EXPORT_DIR)
+    notebooks_dir = os.path.join(os.path.dirname(dest_path), "notebooks")
+    basename = os.path.basename(dest_path).replace(".html", ".ipynb")
+    return os.path.join(notebooks_dir, basename)
+
+@contextlib.contextmanager
+def set_umask(new_mask):
+    previous = os.umask(new_mask)
+    try:
+        yield
+    finally:
+        os.umask(previous)
+
 def script_post_save(model, os_path, contents_manager, **kwargs):
     """convert notebooks to Python script after save with nbconvert
 
@@ -42,13 +68,23 @@ def script_post_save(model, os_path, contents_manager, **kwargs):
 
     log = contents_manager.log
 
-    base, ext = os.path.splitext(os_path)
-    script, resources = _script_exporter.from_filename(os_path)
-    script_fname = os.path.join(EXPORT_DIR, strip_prefix(base, "/home/") + resources.get('output_extension', '.txt'))
+    script, _ = _script_exporter.from_filename(os_path)
+    script_fname = find_dest_path(os_path, EXPORT_DIR)
     create_parent_dirs(script_fname)
-    log.info("Saving exported HTML: " + str(script_fname))
 
+    log.info(f"Saving exported HTML: {script_fname}")
     with io.open(script_fname, 'w', encoding='utf-8') as f:
         f.write(script)
 
-c.FileContentsManager.post_save_hook = script_post_save
+    symlink_path = find_symlink_path(os_path)
+    if not os.path.exists(symlink_path):
+        create_parent_dirs(symlink_path)
+        log.info(f"Symlinking notebook to: {symlink_path}")
+        os.symlink(os.path.abspath(os_path), symlink_path)
+
+def script_with_umask(*args, **kwargs):
+    with set_umask(0):
+        result = script_post_save(*args, **kwargs)
+    return result
+
+c.FileContentsManager.post_save_hook = script_with_umask
