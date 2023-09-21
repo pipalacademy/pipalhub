8c6f626c98b5639c9e8512eda4a073af8a4b4097
Anand Chitipothu <anandology@gmail.com>
2023-09-21 06:31:54 +0530

    Look for the notebooks from /home/jupyter-*
    This is temporary fix to make it work for the upcoming training.
    The paths are curently hard coded. These need to be moved to a configuration.

--- preview/app.py
+++ preview/app.py
@@ -1,28 +1,39 @@
 from flask import Flask, render_template, send_from_directory, jsonify
 from pathlib import Path
 import ipytail
+import glob
 
 app = Flask(__name__)
 
+notebook_name = "session-01.ipynb"
+notebook_pattern = f"/home/jupyter-*/{notebook_name}"
+
+def get_notebook_path(name):
+    return f"/home/jupyter-{name}/{notebook_name}"
+
+def get_name(path):
+    path = Path(path)
+    return path.parent.name.replace("jupyter-", "")
+
 @app.route("/")
 def index():
-    notebooks = Path("notebooks").glob("*.ipynb")
-    return render_template("index.html", notebooks=list(notebooks))
+    paths = glob.glob(notebook_pattern)
+    names = [get_name(path) for path in paths]
+    return render_template("index.html", notebooks=names)
 
 @app.route("/nb/<name>.ipynb")
 def notebook_ipynb(name):
-    path = name + ".ipynb"
-    return send_from_directory('notebooks', path)
+    path = Path(get_notebook_path(name))
+    return send_from_directory(path.parent, path.name)
 
 
 @app.route("/nbtail/<name>.ipynb")
 def notebook_tail(name):
-    path = Path("notebooks") / (name + ".ipynb")
+    path = get_notebook_path(name)
 
     tail = ipytail.IPyTail()
     nb = tail.process_file(path)
     return jsonify(nb)
-
 
 @app.route("/nb/<name>")
 def notebook(name):
